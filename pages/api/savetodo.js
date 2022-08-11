46741cd56893eae00cb1fec521e5472ef2ddcc7e
dadhy <evgenzolotoff@gmail.com>
2022-08-11 21:35:25 +0400

    path

--- pages/api/savetodo.js
+++ pages/api/savetodo.js
@@ -2,7 +2,7 @@ import * as fs from 'fs/promises'
 
 export default async function saveTodo(req, res) {
   const todoList = req.query.text
-  await fs.writeFile('/todoList.json', todoList)
+  await fs.writeFile('/todo/todoList.json', todoList)
 
   res.status(200).json({ name: 'John Doe' })
 }
